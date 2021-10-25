ba593ad39fcccd7220411cfb2d3a95540d2e942f
JanaZhakPl <janazhakpl@gmail.com>
2021-10-25 08:37:53 +0200

    добавлен пробел в переменную fullness;

--- src/it/campe/Main.java
+++ src/it/campe/Main.java
@@ -3,7 +3,7 @@ package it.campe;
 public class Main {
 
     public static void main(String[] args) {
-        String emptiness = "пустота ";
+        String emptiness = "пустота";
         String fullness = emptiness + " " + emptiness +  " " + emptiness; //напишите тут ваш код
         System.out.println(fullness);
     }
